24ab720d2a38503129718099fd8fe615a932a4be
francis <willenerfrancis0@gmail.com>
2022-09-21 19:21:11 -0300

    Interaccion entre FrontEnd y ApiRest terminada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Importamos los componentes que luego pasaremos a nuestras rutas
+import { GameListComponent } from './components/game-list/game-list.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
+
+// Creacion de rutas del frontend
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/games',
+    pathMatch: 'full'
+  },
+  {
+    path: 'games',
+    component: GameListComponent
+  },
+  {
+    path: 'games/add',
+    component: GameFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
